20587ecf4c552c65a1685288f2ce51c7f8b0cb2a
freakbyte <freakbyte@gmail.com>
2020-11-12 21:32:13 +0100

    Initial commit
    Set up basic cross platform environment and ui using imgui

---
+++ Brutus/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+using ImGuiNET;
+using Veldrid;
+using Veldrid.Sdl2;
+using Veldrid.StartupUtilities;
+
+namespace Brutus
+{
+    class Program
+    {
+        private static Sdl2Window window;
+        private static GraphicsDevice gd;
+        private static CommandList cl;
+        private static ImGuiController controller;
+        
+        private static readonly Vector3 ClearColor = new Vector3(0f, 0f, 0f);
+        static void SetThing(out float i, float val) { i = val; }
+        
+        static void Main(string[] args)
+        {
+            // Create window, GraphicsDevice, and all resources necessary for the demo.
+            VeldridStartup.CreateWindowAndGraphicsDevice(
+                new WindowCreateInfo(50, 50, 1280, 720, WindowState.Normal, "Brutus"),
+                new GraphicsDeviceOptions(true, null, true),
+                out window,
+                out gd);
+            
+            window.Resized += () =>
+            {
+                gd.MainSwapchain.Resize((uint)window.Width, (uint)window.Height);
+                controller.WindowResized(window.Width, window.Height);
+            };
+            cl = gd.ResourceFactory.CreateCommandList();
+            controller = new ImGuiController(gd, gd.MainSwapchain.Framebuffer.OutputDescription, window.Width, window.Height);
+            
+            // Main application loop
+            while (window.Exists)
+            {
+                InputSnapshot snapshot = window.PumpEvents();
+                if (!window.Exists) { break; }
+                controller.Update(1f / 60f, snapshot); // Feed the input events to our ImGui controller, which passes them through to ImGui.
+
+                SubmitUi();
+
+                cl.Begin();
+                cl.SetFramebuffer(gd.MainSwapchain.Framebuffer);
+                cl.ClearColorTarget(0, new RgbaFloat(ClearColor.X, ClearColor.Y, ClearColor.Z, 1f));
+                controller.Render(gd, cl);
+                cl.End();
+                gd.SubmitCommands(cl);
+                gd.SwapBuffers(gd.MainSwapchain);
+            }
+
+            // Clean up Veldrid resources
+            gd.WaitForIdle();
+            controller.Dispose();
+            cl.Dispose();
+            gd.Dispose();
+        }
+        
+        private static unsafe void SubmitUi()
+        {
+            ImGui.SetNextWindowPos(new Vector2(0, 0));
+            ImGui.SetNextWindowSize(window.Bounds.Size);
+            ImGui.PushStyleVar(ImGuiStyleVar.WindowRounding, 0);
+            ImGui.Begin("Brutus", ImGuiWindowFlags.NoDecoration | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoResize);
+
+            ImGui.Text("Here be dragons!");
+            ImGui.End();
+            
+            ImGuiIOPtr io = ImGui.GetIO();
+            SetThing(out io.DeltaTime, 2f);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Brutus
+
+*This project was created for educational purposes only and should not be used by anyone.*
